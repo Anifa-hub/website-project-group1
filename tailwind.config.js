6273517987a3356887a80684b1d0aa3a16156c13
Reponse <reponse.iduha2023@kepler.org>
2025-08-22 16:27:34 +0200

    Removed sm breakpoint from the contact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-<<<<<<< HEAD
       fontFamily:{
         outfit:['Outfit', 'sans-serif'],
-=======
-      fontFamily: {
-        outfit: ["Outfit","sans-serif"]
-
->>>>>>> 1d55c011b769391377b0e359a66a4b9acf90565d
+      },
+      fontColor:{
+        
       }
     },
   },
